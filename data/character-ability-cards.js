547f0e46e563b59a2a9a5f7df30166a6a3039b3e
any2cards <any2cards@yahoo.com>
2020-03-25 19:49:35 -0700

    JCV Initial Implementation Files

---
+++ data/character-ability-cards.js
@@ -0,0 +1,482 @@
+[
+  {
+    "name": "crash protocol",
+    "points": 0,
+    "image": "character-ability-cards/DE/crash-protocol.png",
+    "xws": "crashprotocol"
+  },
+  {
+    "name": "crushing weight",
+    "points": 1,
+    "image": "character-ability-cards/DE/crushing-weight.png",
+    "xws": "crushingweight"
+  },
+  {
+    "name": "de-back",
+    "points": 2,
+    "image": "character-ability-cards/DE/de-back.png",
+    "xws": "deback"
+  },
+  {
+    "name": "explode",
+    "points": 3,
+    "image": "character-ability-cards/DE/explode.png",
+    "xws": "explode"
+  },
+  {
+    "name": "explosive blitz",
+    "points": 4,
+    "image": "character-ability-cards/DE/explosive-blitz.png",
+    "xws": "explosiveblitz"
+  },
+  {
+    "name": "extra fuel",
+    "points": 5,
+    "image": "character-ability-cards/DE/extra-fuel.png",
+    "xws": "extrafuel"
+  },
+  {
+    "name": "fire bombs",
+    "points": 6,
+    "image": "character-ability-cards/DE/fire-bombs.png",
+    "xws": "firebombs"
+  },
+  {
+    "name": "implode",
+    "points": 7,
+    "image": "character-ability-cards/DE/implode.png",
+    "xws": "implode"
+  },
+  {
+    "name": "knock out the support",
+    "points": 8,
+    "image": "character-ability-cards/DE/knock-out-the-support.png",
+    "xws": "knockoutthesupport"
+  },
+  {
+    "name": "level",
+    "points": 9,
+    "image": "character-ability-cards/DE/level.png",
+    "xws": "level"
+  },
+  {
+    "name": "lobbed charge",
+    "points": 10,
+    "image": "character-ability-cards/DE/lobbed-charge.png",
+    "xws": "lobbedcharge"
+  },
+  {
+    "name": "nowhere to run",
+    "points": 11,
+    "image": "character-ability-cards/DE/nowhere-to-run.png",
+    "xws": "nowheretorun"
+  },
+  {
+    "name": "one-two punch",
+    "points": 12,
+    "image": "character-ability-cards/DE/one-two-punch.png",
+    "xws": "onetwopunch"
+  },
+  {
+    "name": "piston punch",
+    "points": 13,
+    "image": "character-ability-cards/DE/piston-punch.png",
+    "xws": "pistonpunch"
+  },
+  {
+    "name": "remote detonation",
+    "points": 14,
+    "image": "character-ability-cards/DE/remote-detonation.png",
+    "xws": "remotedetonation"
+  },
+  {
+    "name": "robotic enhancement",
+    "points": 15,
+    "image": "character-ability-cards/DE/robotic-enhancement.png",
+    "xws": "roboticenhancement"
+  },
+  {
+    "name": "rubble",
+    "points": 16,
+    "image": "character-ability-cards/DE/rubble.png",
+    "xws": "rubble"
+  },
+  {
+    "name": "the big one",
+    "points": 17,
+    "image": "character-ability-cards/DE/the-big-one.png",
+    "xws": "thebigone"
+  },
+  {
+    "name": "windup",
+    "points": 18,
+    "image": "character-ability-cards/DE/windup.png",
+    "xws": "windup"
+  },
+  {
+    "name": "care package",
+    "points": 19,
+    "image": "character-ability-cards/HA/care-package.png",
+    "xws": "carepackage"
+  },
+  {
+    "name": "center mass",
+    "points": 20,
+    "image": "character-ability-cards/HA/center-mass.png",
+    "xws": "centermass"
+  },
+  {
+    "name": "close cuts",
+    "points": 21,
+    "image": "character-ability-cards/HA/close-cuts.png",
+    "xws": "closecuts"
+  },
+  {
+    "name": "disorienting barrage",
+    "points": 22,
+    "image": "character-ability-cards/HA/disorienting-barrage.png",
+    "xws": "disorientingbarrage"
+  },
+  {
+    "name": "double throw",
+    "points": 23,
+    "image": "character-ability-cards/HA/double-throw.png",
+    "xws": "doublethrow"
+  },
+  {
+    "name": "extra lift",
+    "points": 24,
+    "image": "character-ability-cards/HA/extra-lift.png",
+    "xws": "extralift"
+  },
+  {
+    "name": "fancy hat",
+    "points": 25,
+    "image": "character-ability-cards/HA/fancy-hat.png",
+    "xws": "fancyhat"
+  },
+  {
+    "name": "fearsome efficiency",
+    "points": 26,
+    "image": "character-ability-cards/HA/fearsome-efficiency.png",
+    "xws": "fearsomeefficiency"
+  },
+  {
+    "name": "follow through",
+    "points": 27,
+    "image": "character-ability-cards/HA/follow-through.png",
+    "xws": "followthrough"
+  },
+  {
+    "name": "ha-back",
+    "points": 28,
+    "image": "character-ability-cards/HA/ha-back.png",
+    "xws": "haback"
+  },
+  {
+    "name": "overwatch",
+    "points": 29,
+    "image": "character-ability-cards/HA/overwatch.png",
+    "xws": "overwatch"
+  },
+  {
+    "name": "power pitch",
+    "points": 30,
+    "image": "character-ability-cards/HA/power-pitch.png",
+    "xws": "powerpitch"
+  },
+  {
+    "name": "repeat shot",
+    "points": 31,
+    "image": "character-ability-cards/HA/repeat-shot.png",
+    "xws": "repeatshot"
+  },
+  {
+    "name": "retrieval",
+    "points": 32,
+    "image": "character-ability-cards/HA/retrieval.png",
+    "xws": "retrieval"
+  },
+  {
+    "name": "ricochet",
+    "points": 33,
+    "image": "character-ability-cards/HA/ricochet.png",
+    "xws": "ricochet"
+  },
+  {
+    "name": "ripped from the flesh",
+    "points": 34,
+    "image": "character-ability-cards/HA/ripped-from-the-flesh.png",
+    "xws": "rippedfromtheflesh"
+  },
+  {
+    "name": "second wind",
+    "points": 35,
+    "image": "character-ability-cards/HA/second-wind.png",
+    "xws": "secondwind"
+  },
+  {
+    "name": "sharpened blades",
+    "points": 36,
+    "image": "character-ability-cards/HA/sharpened-blades.png",
+    "xws": "sharpenedblades"
+  },
+  {
+    "name": "stopping power",
+    "points": 37,
+    "image": "character-ability-cards/HA/stopping-power.png",
+    "xws": "stoppingpower"
+  },
+  {
+    "name": "the favorite",
+    "points": 38,
+    "image": "character-ability-cards/HA/the-favorite.png",
+    "xws": "thefavorite"
+  },
+  {
+    "name": "barbaric instincts",
+    "points": 39,
+    "image": "character-ability-cards/RG/barbaric-instincts.png",
+    "xws": "barbaricinstincts"
+  },
+  {
+    "name": "blade dance",
+    "points": 40,
+    "image": "character-ability-cards/RG/blade-dance.png",
+    "xws": "bladedance"
+  },
+  {
+    "name": "blinding sickle",
+    "points": 41,
+    "image": "character-ability-cards/RG/blinding-sickle.png",
+    "xws": "blindingsickle"
+  },
+  {
+    "name": "burn away the dark",
+    "points": 42,
+    "image": "character-ability-cards/RG/burn-away-the-dark.png",
+    "xws": "burnawaythedark"
+  },
+  {
+    "name": "desert night",
+    "points": 43,
+    "image": "character-ability-cards/RG/desert-night.png",
+    "xws": "desertnight"
+  },
+  {
+    "name": "flame shroud",
+    "points": 44,
+    "image": "character-ability-cards/RG/flame-shroud.png",
+    "xws": "flameshroud"
+  },
+  {
+    "name": "flaming sickle",
+    "points": 45,
+    "image": "character-ability-cards/RG/flaming-sickle.png",
+    "xws": "flamingsickle"
+  },
+  {
+    "name": "harvest sickle",
+    "points": 46,
+    "image": "character-ability-cards/RG/harvest-sickle.png",
+    "xws": "harvestsickle"
+  },
+  {
+    "name": "healing sands",
+    "points": 47,
+    "image": "character-ability-cards/RG/healing-sands.png",
+    "xws": "healingsands"
+  },
+  {
+    "name": "precision strike",
+    "points": 48,
+    "image": "character-ability-cards/RG/precision-strike.png",
+    "xws": "precisionstrike"
+  },
+  {
+    "name": "radiant sickle",
+    "points": 49,
+    "image": "character-ability-cards/RG/radiant-sickle.png",
+    "xws": "radiantsickle"
+  },
+  {
+    "name": "rg-back",
+    "points": 50,
+    "image": "character-ability-cards/RG/rg-back.png",
+    "xws": "rgback"
+  },
+  {
+    "name": "shield of the desert",
+    "points": 51,
+    "image": "character-ability-cards/RG/shield-of-the-desert.png",
+    "xws": "shieldofthedesert"
+  },
+  {
+    "name": "shield spikes",
+    "points": 52,
+    "image": "character-ability-cards/RG/shield-spikes.png",
+    "xws": "shieldspikes"
+  },
+  {
+    "name": "shocking advance",
+    "points": 53,
+    "image": "character-ability-cards/RG/shocking-advance.png",
+    "xws": "shockingadvance"
+  },
+  {
+    "name": "strangling chain",
+    "points": 54,
+    "image": "character-ability-cards/RG/strangling-chain.png",
+    "xws": "stranglingchain"
+  },
+  {
+    "name": "swift strength",
+    "points": 55,
+    "image": "character-ability-cards/RG/swift-strength.png",
+    "xws": "swiftstrength"
+  },
+  {
+    "name": "twirling stabs",
+    "points": 56,
+    "image": "character-ability-cards/RG/twirling-stabs.png",
+    "xws": "twirlingstabs"
+  },
+  {
+    "name": "warmth of the sun",
+    "points": 57,
+    "image": "character-ability-cards/RG/warmth-of-the-sun.png",
+    "xws": "warmthofthesun"
+  },
+  {
+    "name": "warrior of the sun",
+    "points": 58,
+    "image": "character-ability-cards/RG/warrior-of-the-sun.png",
+    "xws": "warriorofthesun"
+  },
+  {
+    "name": "black boon",
+    "points": 59,
+    "image": "character-ability-cards/VW/black-boon.png",
+    "xws": "blackboon"
+  },
+  {
+    "name": "close to the abyss",
+    "points": 60,
+    "image": "character-ability-cards/VW/close-to-the-abyss.png",
+    "xws": "closetotheabyss"
+  },
+  {
+    "name": "cold embrace",
+    "points": 61,
+    "image": "character-ability-cards/VW/cold-embrace.png",
+    "xws": "coldembrace"
+  },
+  {
+    "name": "commanding presence",
+    "points": 62,
+    "image": "character-ability-cards/VW/commanding-presence.png",
+    "xws": "commandingpresence"
+  },
+  {
+    "name": "crushing cold",
+    "points": 63,
+    "image": "character-ability-cards/VW/crushing-cold.png",
+    "xws": "crushingcold"
+  },
+  {
+    "name": "freeze the soul",
+    "points": 64,
+    "image": "character-ability-cards/VW/freeze-the-soul.png",
+    "xws": "freezethesoul"
+  },
+  {
+    "name": "gift of the void",
+    "points": 65,
+    "image": "character-ability-cards/VW/gift-of-the-void.png",
+    "xws": "giftofthevoid"
+  },
+  {
+    "name": "give and take",
+    "points": 66,
+    "image": "character-ability-cards/VW/give-and-take.png",
+    "xws": "giveandtake"
+  },
+  {
+    "name": "grasp of doom",
+    "points": 67,
+    "image": "character-ability-cards/VW/grasp-of-doom.png",
+    "xws": "graspofdoom"
+  },
+  {
+    "name": "lure of the void",
+    "points": 68,
+    "image": "character-ability-cards/VW/lure-of-the-void.png",
+    "xws": "lureofthevoid"
+  },
+  {
+    "name": "master influence",
+    "points": 69,
+    "image": "character-ability-cards/VW/master-influence.png",
+    "xws": "masterinfluence"
+  },
+  {
+    "name": "resigned frenzy",
+    "points": 70,
+    "image": "character-ability-cards/VW/resigned-frenzy.png",
+    "xws": "resignedfrenzy"
+  },
+  {
+    "name": "sap warmth",
+    "points": 71,
+    "image": "character-ability-cards/VW/sap-warmth.png",
+    "xws": "sapwarmth"
+  },
+  {
+    "name": "savage instinct",
+    "points": 72,
+    "image": "character-ability-cards/VW/savage-instinct.png",
+    "xws": "savageinstinct"
+  },
+  {
+    "name": "signs of the void",
+    "points": 73,
+    "image": "character-ability-cards/VW/signs-of-the-void.png",
+    "xws": "signsofthevoid"
+  },
+  {
+    "name": "suggestion",
+    "points": 74,
+    "image": "character-ability-cards/VW/suggestion.png",
+    "xws": "suggestion"
+  },
+  {
+    "name": "taunting fate",
+    "points": 75,
+    "image": "character-ability-cards/VW/taunting-fate.png",
+    "xws": "tauntingfate"
+  },
+  {
+    "name": "the last journey",
+    "points": 76,
+    "image": "character-ability-cards/VW/the-last-journey.png",
+    "xws": "thelastjourney"
+  },
+  {
+    "name": "turn out the lights",
+    "points": 77,
+    "image": "character-ability-cards/VW/turn-out-the-lights.png",
+    "xws": "turnoutthelights"
+  },
+  {
+    "name": "vw-back",
+    "points": 78,
+    "image": "character-ability-cards/VW/vw-back.png",
+    "xws": "vwback"
+  },
+  {
+    "name": "wicked scratch",
+    "points": 79,
+    "image": "character-ability-cards/VW/wicked-scratch.png",
+    "xws": "wickedscratch"
+  }
+]
---
+++ data/character-mats.js
@@ -0,0 +1,50 @@
+[
+  {
+    "name": "demolitionist",
+    "points": 0,
+    "image": "character-mats/demolitionist.png",
+    "xws": "demolitionist"
+  },
+  {
+    "name": "demolitionist",
+    "points": 1,
+    "image": "character-mats/demolitionist-back.png",
+    "xws": "demolitionist"
+  },
+  {
+    "name": "hatchet",
+    "points": 2,
+    "image": "character-mats/hatchet.png",
+    "xws": "hatchet"
+  },
+  {
+    "name": "hatchet",
+    "points": 3,
+    "image": "character-mats/hatchet-back.png",
+    "xws": "hatchet"
+  },
+  {
+    "name": "red guard",
+    "points": 4,
+    "image": "character-mats/red-guard.png",
+    "xws": "redguard"
+  },
+  {
+    "name": "red guard",
+    "points": 5,
+    "image": "character-mats/red-guard-back.png",
+    "xws": "redguard"
+  },
+  {
+    "name": "voidwarden",
+    "points": 6,
+    "image": "character-mats/voidwarden.png",
+    "xws": "voidwarden"
+  },
+  {
+    "name": "voidwarden",
+    "points": 7,
+    "image": "character-mats/voidwarden-back.png",
+    "xws": "voidwarden"
+  }
+]
