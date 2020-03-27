b9d057965b0bb09f10441b70e1530a17ac04d229
any2cards <any2cards@yahoo.com>
2020-03-27 08:07:37 -0700

    Implement Character Perks

---
+++ data/character-perks.js
@@ -0,0 +1,26 @@
+[
+  {
+    "name": "demolitionist perks",
+    "points": 0,
+    "image": "character-perks/demolitionist-perks.png",
+    "xws": "demolitionistperks"
+  },
+  {
+    "name": "hatchet perks",
+    "points": 1,
+    "image": "character-perks/hatchet-perks.png",
+    "xws": "hatchetperks"
+  },
+  {
+    "name": "red guard perks",
+    "points": 2,
+    "image": "character-perks/red-guard-perks.png",
+    "xws": "redguardperks"
+  },
+  {
+    "name": "voidwarden perks",
+    "points": 3,
+    "image": "character-perks/voidwarden-perks.png",
+    "xws": "voidwardenperks"
+  }
+]
