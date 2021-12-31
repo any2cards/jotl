ab3621a304c06d1f3814d71df72d3343e182877b
any2cards <any2cards@yahoo.com>
2021-12-31 09:32:14 -0800

    Implement STD CP

--- data/character-perks.js
+++ data/character-perks.js
@@ -2,25 +2,29 @@
   {
     "name": "demolitionist perks",
     "points": 0,
-    "image": "character-perks/demolitionist-perks.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-perks/jaws-of-the-lion/jl-demolitionist-perks.png",
     "xws": "demolitionistperks"
   },
   {
     "name": "hatchet perks",
     "points": 1,
-    "image": "character-perks/hatchet-perks.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-perks/jaws-of-the-lion/jl-hatchet-perks.png",
     "xws": "hatchetperks"
   },
   {
     "name": "red guard perks",
     "points": 2,
-    "image": "character-perks/red-guard-perks.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-perks/jaws-of-the-lion/jl-red-guard-perks.png",
     "xws": "redguardperks"
   },
   {
     "name": "voidwarden perks",
     "points": 3,
-    "image": "character-perks/voidwarden-perks.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-perks/jaws-of-the-lion/jl-voidwarden-perks.png",
     "xws": "voidwardenperks"
   }
 ]
