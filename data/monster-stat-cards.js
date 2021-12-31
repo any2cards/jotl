d7e34ef3972bbf21abef8f3bb94de4b18839a140
any2cards <any2cards@yahoo.com>
2021-12-31 10:34:21 -0800

    Implement STD MS

--- data/monster-stat-cards.js
+++ data/monster-stat-cards.js
@@ -2,205 +2,239 @@
   {
     "name": "black imp",
     "points": 0,
-    "image": "monster-stat-cards/black-imp-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-black-imp-0.png",
     "xws": "blackimp"
   },
   {
     "name": "black imp",
     "points": 1,
-    "image": "monster-stat-cards/black-imp-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-black-imp-4.png",
     "xws": "blackimp"
   },
   {
     "name": "black sludge",
     "points": 2,
-    "image": "monster-stat-cards/black-sludge-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-black-sludge-0.png",
     "xws": "blacksludge"
   },
   {
     "name": "black sludge",
     "points": 3,
-    "image": "monster-stat-cards/black-sludge-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-black-sludge-4.png",
     "xws": "blacksludge"
   },
   {
     "name": "blood horror",
     "points": 4,
-    "image": "monster-stat-cards/blood-horror-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-blood-horror-0.png",
     "xws": "bloodhorror"
   },
   {
     "name": "blood horror",
     "points": 5,
-    "image": "monster-stat-cards/blood-horror-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-blood-horror-4.png",
     "xws": "bloodhorror"
   },
   {
     "name": "blood imp",
     "points": 6,
-    "image": "monster-stat-cards/blood-imp-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-blood-imp-0.png",
     "xws": "bloodimp"
   },
   {
     "name": "blood imp",
     "points": 7,
-    "image": "monster-stat-cards/blood-imp-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-blood-imp-4.png",
     "xws": "bloodimp"
   },
   {
     "name": "blood monstrosity",
     "points": 8,
-    "image": "monster-stat-cards/blood-monstrosity-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-blood-monstrosity-0.png",
     "xws": "bloodmonstrosity"
   },
   {
     "name": "blood monstrosity",
     "points": 9,
-    "image": "monster-stat-cards/blood-monstrosity-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-blood-monstrosity-4.png",
     "xws": "bloodmonstrosity"
   },
   {
     "name": "blood tumor",
     "points": 10,
-    "image": "monster-stat-cards/blood-tumor-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-blood-tumor-0.png",
     "xws": "bloodtumor"
   },
   {
     "name": "blood tumor",
     "points": 11,
-    "image": "monster-stat-cards/blood-tumor-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-blood-tumor-4.png",
     "xws": "bloodtumor"
   },
   {
     "name": "chaos demon",
     "points": 12,
-    "image": "monster-stat-cards/chaos-demon-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-chaos-demon-0.png",
     "xws": "chaosdemon"
   },
   {
     "name": "chaos demon",
     "points": 13,
-    "image": "monster-stat-cards/chaos-demon-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-chaos-demon-4.png",
     "xws": "chaosdemon"
   },
   {
     "name": "first of the order",
     "points": 14,
-    "image": "monster-stat-cards/first-of-the-order-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-first-of-the-order-0.png",
     "xws": "firstoftheorder"
   },
   {
     "name": "first of the order",
     "points": 15,
-    "image": "monster-stat-cards/first-of-the-order-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-first-of-the-order-4.png",
     "xws": "firstoftheorder"
   },
   {
     "name": "giant viper",
     "points": 16,
-    "image": "monster-stat-cards/giant-viper-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-giant-viper-0.png",
     "xws": "giantviper"
   },
   {
     "name": "giant viper",
     "points": 17,
-    "image": "monster-stat-cards/giant-viper-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-giant-viper-4.png",
     "xws": "giantviper"
   },
   {
     "name": "living corpse",
     "points": 18,
-    "image": "monster-stat-cards/living-corpse-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-living-corpse-0.png",
     "xws": "livingcorpse"
   },
   {
     "name": "living corpse",
     "points": 19,
-    "image": "monster-stat-cards/living-corpse-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-living-corpse-4.png",
     "xws": "livingcorpse"
   },
   {
     "name": "living spirit",
     "points": 20,
-    "image": "monster-stat-cards/living-spirit-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-living-spirit-0.png",
     "xws": "livingspirit"
   },
   {
     "name": "living spirit",
     "points": 21,
-    "image": "monster-stat-cards/living-spirit-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-living-spirit-4.png",
     "xws": "livingspirit"
   },
   {
     "name": "monster stat card envelope 6",
     "points": 22,
-    "image": "monster-stat-cards/monster-stat-card-envelope-6.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-monster-stat-card-envelope-6.png",
     "xws": "monsterstatcardenvelope6"
   },
   {
     "name": "monster stat card envelope 10",
     "points": 23,
-    "image": "monster-stat-cards/monster-stat-card-envelope-10.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-monster-stat-card-envelope-10.png",
     "xws": "monsterstatcardenvelope6"
   },
   {
     "name": "rat monstrosity",
     "points": 24,
-    "image": "monster-stat-cards/rat-monstrosity-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-rat-monstrosity-0.png",
     "xws": "ratmonstrosity"
   },
   {
     "name": "rat monstrosity",
     "points": 25,
-    "image": "monster-stat-cards/rat-monstrosity-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-rat-monstrosity-4.png",
     "xws": "ratmonstrosity"
   },
   {
     "name": "stone golem",
     "points": 26,
-    "image": "monster-stat-cards/stone-golem-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-stone-golem-0.png",
     "xws": "stonegolem"
   },
   {
     "name": "stone golem",
     "points": 27,
-    "image": "monster-stat-cards/stone-golem-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-stone-golem-4.png",
     "xws": "stonegolem"
   },
   {
     "name": "vermling raider",
     "points": 28,
-    "image": "monster-stat-cards/vermling-raider-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-vermling-raider-0.png",
     "xws": "vermlingraider"
   },
   {
     "name": "vermling raider",
     "points": 29,
-    "image": "monster-stat-cards/vermling-raider-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-vermling-raider-4.png",
     "xws": "vermlingraider"
   },
   {
     "name": "vermling scout",
     "points": 30,
-    "image": "monster-stat-cards/vermling-scout-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-vermling-scout-0.png",
     "xws": "vermlingscout"
   },
   {
     "name": "vermling scout",
     "points": 31,
-    "image": "monster-stat-cards/vermling-scout-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-vermling-scout-4.png",
     "xws": "vermlingscout"
   },
   {
     "name": "zealot",
     "points": 32,
-    "image": "monster-stat-cards/zealot-0.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-zealot-0.png",
     "xws": "zealot"
   },
   {
     "name": "zealot",
     "points": 33,
-    "image": "monster-stat-cards/zealot-4.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "monster-stat-cards/jaws-of-the-lion/jl-zealot-4.png",
     "xws": "zealot"
   }
 ]
