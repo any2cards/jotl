26783cc4d467c8d77469d04d91ced613b26ce426
any2cards <any2cards@yahoo.com>
2021-12-31 09:27:11 -0800

    Implement STD CM

--- data/character-mats.js
+++ data/character-mats.js
@@ -2,49 +2,57 @@
   {
     "name": "demolitionist",
     "points": 0,
-    "image": "character-mats/demolitionist-front.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-mats/jaws-of-the-lion/jl-demolitionist-front.png",
     "xws": "demolitionist"
   },
   {
     "name": "demolitionist",
     "points": 1,
-    "image": "character-mats/demolitionist-back.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-mats/jaws-of-the-lion/jl-demolitionist-back.png",
     "xws": "demolitionist"
   },
   {
     "name": "hatchet",
     "points": 2,
-    "image": "character-mats/hatchet-front.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-mats/jaws-of-the-lion/jl-hatchet-front.png",
     "xws": "hatchet"
   },
   {
     "name": "hatchet",
     "points": 3,
-    "image": "character-mats/hatchet-back.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-mats/jaws-of-the-lion/jl-hatchet-back.png",
     "xws": "hatchet"
   },
   {
     "name": "red guard",
     "points": 4,
-    "image": "character-mats/red-guard-front.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-mats/jaws-of-the-lion/jl-red-guard-front.png",
     "xws": "redguard"
   },
   {
     "name": "red guard",
     "points": 5,
-    "image": "character-mats/red-guard-back.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-mats/jaws-of-the-lion/jl-red-guard-back.png",
     "xws": "redguard"
   },
   {
     "name": "voidwarden",
     "points": 6,
-    "image": "character-mats/voidwarden-front.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-mats/jaws-of-the-lion/jl-voidwarden-front.png",
     "xws": "voidwarden"
   },
   {
     "name": "voidwarden",
     "points": 7,
-    "image": "character-mats/voidwarden-back.png",
+    "expansion": "Jaws Of The Lion",
+    "image": "character-mats/jaws-of-the-lion/jl-voidwarden-back.png",
     "xws": "voidwarden"
   }
 ]
