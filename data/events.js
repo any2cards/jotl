e74a30bb6e5c1235fb421d0a55451d65138c35dc
any2cards <any2cards@yahoo.com>
2021-12-31 09:38:27 -0800

    Implement STD Events

--- data/events.js
+++ data/events.js
@@ -1,266 +1,310 @@
 [
   {
-    "name": "ce-01-b",
+    "name": "jl-ce-01-b",
     "points": 0,
-    "image": "events/city/ce-01-b.png",
-    "xws": "ce01b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-01-b.png",
+    "xws": "jlce01b"
   },
   {
-    "name": "ce-01-f",
+    "name": "jl-ce-01-f",
     "points": 1,
-    "image": "events/city/ce-01-f.png",
-    "xws": "ce01f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-01-f.png",
+    "xws": "jlce01f"
   },
   {
-    "name": "ce-02-b",
+    "name": "jl-ce-02-b",
     "points": 2,
-    "image": "events/city/ce-02-b.png",
-    "xws": "ce02b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-02-b.png",
+    "xws": "jlce02b"
   },
   {
-    "name": "ce-02-f",
+    "name": "jl-ce-02-f",
     "points": 3,
-    "image": "events/city/ce-02-f.png",
-    "xws": "ce02f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-02-f.png",
+    "xws": "jlce02f"
   },
   {
-    "name": "ce-03-b",
+    "name": "jl-ce-03-b",
     "points": 4,
-    "image": "events/city/ce-03-b.png",
-    "xws": "ce03b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-03-b.png",
+    "xws": "jlce03b"
   },
   {
-    "name": "ce-03-f",
+    "name": "jl-ce-03-f",
     "points": 5,
-    "image": "events/city/ce-03-f.png",
-    "xws": "ce03f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-03-f.png",
+    "xws": "jlce03f"
   },
   {
-    "name": "ce-04-b",
+    "name": "jl-ce-04-b",
     "points": 6,
-    "image": "events/city/ce-04-b.png",
-    "xws": "ce04b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-04-b.png",
+    "xws": "jlce04b"
   },
   {
-    "name": "ce-04-f",
+    "name": "jl-ce-04-f",
     "points": 7,
-    "image": "events/city/ce-04-f.png",
-    "xws": "ce04f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-04-f.png",
+    "xws": "jlce04f"
   },
   {
-    "name": "ce-05-b",
+    "name": "jl-ce-05-b",
     "points": 8,
-    "image": "events/city/ce-05-b.png",
-    "xws": "ce05b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-05-b.png",
+    "xws": "jlce05b"
   },
   {
-    "name": "ce-05-f",
+    "name": "jl-ce-05-f",
     "points": 9,
-    "image": "events/city/ce-05-f.png",
-    "xws": "ce05f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-05-f.png",
+    "xws": "jlce05f"
   },
   {
-    "name": "ce-06-b",
+    "name": "jl-ce-06-b",
     "points": 10,
-    "image": "events/city/ce-06-b.png",
-    "xws": "ce06b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-06-b.png",
+    "xws": "jlce06b"
   },
   {
-    "name": "ce-06-f",
+    "name": "jl-ce-06-f",
     "points": 11,
-    "image": "events/city/ce-06-f.png",
-    "xws": "ce06f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-06-f.png",
+    "xws": "jlce06f"
   },
   {
-    "name": "ce-07-b",
+    "name": "jl-ce-07-b",
     "points": 12,
-    "image": "events/city/ce-07-b.png",
-    "xws": "ce07b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-07-b.png",
+    "xws": "jlce07b"
   },
   {
-    "name": "ce-07-f",
+    "name": "jl-ce-07-f",
     "points": 13,
-    "image": "events/city/ce-07-f.png",
-    "xws": "ce07f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-07-f.png",
+    "xws": "jlce07f"
   },
   {
-    "name": "ce-08-b",
+    "name": "jl-ce-08-b",
     "points": 14,
-    "image": "events/city/ce-08-b.png",
-    "xws": "ce08b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-08-b.png",
+    "xws": "jlce08b"
   },
   {
-    "name": "ce-08-f",
+    "name": "jl-ce-08-f",
     "points": 15,
-    "image": "events/city/ce-08-f.png",
-    "xws": "ce08f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-08-f.png",
+    "xws": "jlce08f"
   },
   {
-    "name": "ce-09-b",
+    "name": "jl-ce-09-b",
     "points": 16,
-    "image": "events/city/ce-09-b.png",
-    "xws": "ce09b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-09-b.png",
+    "xws": "jlce09b"
   },
   {
-    "name": "ce-09-f",
+    "name": "jl-ce-09-f",
     "points": 17,
-    "image": "events/city/ce-09-f.png",
-    "xws": "ce09f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-09-f.png",
+    "xws": "jlce09f"
   },
   {
-    "name": "ce-10-b",
+    "name": "jl-ce-10-b",
     "points": 18,
-    "image": "events/city/ce-10-b.png",
-    "xws": "ce10b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-10-b.png",
+    "xws": "jlce10b"
   },
   {
-    "name": "ce-10-f",
+    "name": "jl-ce-10-f",
     "points": 19,
-    "image": "events/city/ce-10-f.png",
-    "xws": "ce10f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-10-f.png",
+    "xws": "jlce10f"
   },
   {
-    "name": "ce-11-b",
+    "name": "jl-ce-11-b",
     "points": 20,
-    "image": "events/city/ce-11-b.png",
-    "xws": "ce11b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-11-b.png",
+    "xws": "jlce11b"
   },
   {
-    "name": "ce-11-f",
+    "name": "jl-ce-11-f",
     "points": 21,
-    "image": "events/city/ce-11-f.png",
-    "xws": "ce11f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-11-f.png",
+    "xws": "jlce11f"
   },
   {
-    "name": "ce-12-b",
+    "name": "jl-ce-12-b",
     "points": 22,
-    "image": "events/city/ce-12-b.png",
-    "xws": "ce12b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-12-b.png",
+    "xws": "jlce12b"
   },
   {
-    "name": "ce-12-f",
+    "name": "jl-ce-12-f",
     "points": 23,
-    "image": "events/city/ce-12-f.png",
-    "xws": "ce12f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-12-f.png",
+    "xws": "jlce12f"
   },
   {
-    "name": "ce-13-b",
+    "name": "jl-ce-13-b",
     "points": 24,
-    "image": "events/city/ce-13-b.png",
-    "xws": "ce13b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-13-b.png",
+    "xws": "jlce13b"
   },
   {
-    "name": "ce-13-f",
+    "name": "jl-ce-13-f",
     "points": 25,
-    "image": "events/city/ce-13-f.png",
-    "xws": "ce13f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-13-f.png",
+    "xws": "jlce13f"
   },
   {
-    "name": "ce-14-b",
+    "name": "jl-ce-14-b",
     "points": 26,
-    "image": "events/city/ce-14-b.png",
-    "xws": "ce14b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-14-b.png",
+    "xws": "jlce14b"
   },
   {
-    "name": "ce-14-f",
+    "name": "jl-ce-14-f",
     "points": 27,
-    "image": "events/city/ce-14-f.png",
-    "xws": "ce14f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-14-f.png",
+    "xws": "jlce14f"
   },
   {
-    "name": "ce-15-b",
+    "name": "jl-ce-15-b",
     "points": 28,
-    "image": "events/city/ce-15-b.png",
-    "xws": "ce15b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-15-b.png",
+    "xws": "jlce15b"
   },
   {
-    "name": "ce-15-f",
+    "name": "jl-ce-15-f",
     "points": 29,
-    "image": "events/city/ce-15-f.png",
-    "xws": "ce15f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-15-f.png",
+    "xws": "jlce15f"
   },
   {
-    "name": "ce-16-b",
+    "name": "jl-ce-16-b",
     "points": 30,
-    "image": "events/city/ce-16-b.png",
-    "xws": "ce16b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-16-b.png",
+    "xws": "jlce16b"
   },
   {
-    "name": "ce-16-f",
+    "name": "jl-ce-16-f",
     "points": 31,
-    "image": "events/city/ce-16-f.png",
-    "xws": "ce16f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-16-f.png",
+    "xws": "jlce16f"
   },
   {
-    "name": "ce-17-b",
+    "name": "jl-ce-17-b",
     "points": 32,
-    "image": "events/city/ce-17-b.png",
-    "xws": "ce17b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-17-b.png",
+    "xws": "jlce17b"
   },
   {
-    "name": "ce-17-f",
+    "name": "jl-ce-17-f",
     "points": 33,
-    "image": "events/city/ce-17-f.png",
-    "xws": "ce17f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-17-f.png",
+    "xws": "jlce17f"
   },
   {
-    "name": "ce-18-b",
+    "name": "jl-ce-18-b",
     "points": 34,
-    "image": "events/city/ce-18-b.png",
-    "xws": "ce18b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-18-b.png",
+    "xws": "jlce18b"
   },
   {
-    "name": "ce-18-f",
+    "name": "jl-ce-18-f",
     "points": 35,
-    "image": "events/city/ce-18-f.png",
-    "xws": "ce18f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-18-f.png",
+    "xws": "jlce18f"
   },
   {
-    "name": "ce-19-b",
+    "name": "jl-ce-19-b",
     "points": 36,
-    "image": "events/city/ce-19-b.png",
-    "xws": "ce19b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-19-b.png",
+    "xws": "jlce19b"
   },
   {
-    "name": "ce-19-f",
+    "name": "jl-ce-19-f",
     "points": 37,
-    "image": "events/city/ce-19-f.png",
-    "xws": "ce19f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-19-f.png",
+    "xws": "jlce19f"
   },
   {
-    "name": "ce-20-b",
+    "name": "jl-ce-20-b",
     "points": 38,
-    "image": "events/city/ce-20-b.png",
-    "xws": "ce20b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-20-b.png",
+    "xws": "jlce20b"
   },
   {
-    "name": "ce-20-f",
+    "name": "jl-ce-20-f",
     "points": 39,
-    "image": "events/city/ce-20-f.png",
-    "xws": "ce20f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-20-f.png",
+    "xws": "jlce20f"
   },
   {
-    "name": "ce-21-b",
+    "name": "jl-ce-21-b",
     "points": 40,
-    "image": "events/city/ce-21-b.png",
-    "xws": "ce21b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-21-b.png",
+    "xws": "jlce21b"
   },
   {
-    "name": "ce-21-f",
+    "name": "jl-ce-21-f",
     "points": 41,
-    "image": "events/city/ce-21-f.png",
-    "xws": "ce21f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-21-f.png",
+    "xws": "jlce21f"
   },
   {
-    "name": "ce-22-b",
+    "name": "jl-ce-22-b",
     "points": 42,
-    "image": "events/city/ce-22-b.png",
-    "xws": "ce22b"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-22-b.png",
+    "xws": "jlce22b"
   },
   {
-    "name": "ce-22-f",
+    "name": "jl-ce-22-f",
     "points": 43,
-    "image": "events/city/ce-22-f.png",
-    "xws": "ce22f"
+    "expansion": "Jaws Of The Lion",
+    "image": "events/jaws-of-the-lion/city/jl-ce-22-f.png",
+    "xws": "jlce22f"
   }
 ]
